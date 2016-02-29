375ca6b12fe52c9d01ef25a6e27438325677207b
Catastrophe666 <catastronig@yahoo.com>
2016-02-29 13:09:52 -0600

    Added empty classes for the player and obstacles

---
+++ Scroller-Gdx/core/src/com/mygdx/game/obstacle.java
@@ -0,0 +1,24 @@
+package com.mygdx.game;
+
+//[Taj] This class will have everything obstacle related, its just skeleton
+//code for now.
+public class obstacle
+{
+	// [Taj] Initializer.
+	public obstacle()
+	{
+		
+	}
+	
+	// [Taj] This updates things like coordinates.
+	public void update()
+	{
+		
+	}
+	
+	// [Taj] Render the actual obstacle here.
+	public void render()
+	{
+		
+	}
+}
---
+++ Scroller-Gdx/core/src/com/mygdx/game/player.java
@@ -0,0 +1,24 @@
+package com.mygdx.game;
+
+// [Taj] This class will have everything player related, its just skeleton
+// code for now.
+public class player
+{
+	// [Taj] Initializer.
+	public player()
+	{
+		
+	}
+	
+	// [Taj] This updates things like coordinates.
+	public void update()
+	{
+		
+	}
+	
+	// [Taj] Render the actual player here.
+	public void render()
+	{
+		
+	}
+}
