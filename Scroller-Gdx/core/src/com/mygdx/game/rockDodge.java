28f910bcd72970a4a443ac26b4e5f7c0d592a0d0
Alex Shamrov <Alex Shamrov>
2016-02-28 19:54:48 -0600

    Commented out logo image because it was crashing

--- Scroller-Gdx/core/src/com/mygdx/game/rockDodge.java
+++ Scroller-Gdx/core/src/com/mygdx/game/rockDodge.java
@@ -6,22 +6,28 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class rockDodge extends ApplicationAdapter {
+public class rockDodge extends ApplicationAdapter
+{
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
-	public void create () {
+	public void create()
+	{
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		
+		// [Alex] Commented next line because errors
+		//img = new Texture("badlogic.jpg");
 	}
 
 	@Override
-	public void render () {
+	public void render()
+	{
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		// [Alex] Commented next line because no image is being rendered
+		//batch.draw(img, 0, 0);
 		batch.end();
 	}
 }
