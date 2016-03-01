7a0b31fd072fc50ad02b3b6b1334553abcf5207f
Catastrophe666 <catastronig@yahoo.com>
2016-03-01 16:46:37 -0600

    Added sample text rendering to the UI.

--- Scroller-Gdx/core/src/com/mygdx/game/ui.java
+++ Scroller-Gdx/core/src/com/mygdx/game/ui.java
@@ -1,10 +1,20 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 // [Taj] Do all user interface related stuff.
 public class ui
 {
 	// [Taj] This is a reference of the game that this class can use.
 	rockDodge game;
+	
+	// [Taj] This is our font class, we need this to render text.
+	private BitmapFont font;
+	
+	// [Taj] This is the sprite batch we need this to render stuff.
+	private SpriteBatch batch;
 	
 	// [Taj] Initializer.
 	public ui(rockDodge game)
@@ -12,6 +22,9 @@ public class ui
 		// [Taj] References the main game to this class.
 		this.game = game;
 		
+		// [Taj] Now we initialize memory for our fonts and batch
+		font = new BitmapFont();
+		batch = new SpriteBatch();
 	}
 	
 	// [Taj] This updates things like coordinates.
@@ -23,6 +36,12 @@ public class ui
 	// [Taj] Render the actual player here.
 	public void render()
 	{
+		// [Taj] Rendering in libgdx works a bit differently, you need to surround
+		// your rendering stuff with batch.begin and batch.end
 		
+		batch.begin();
+		font.setColor(Color.WHITE);
+		font.draw(batch, "Hello", 100, 100); // [Taj] Draw.
+		batch.end();
 	}
 }
