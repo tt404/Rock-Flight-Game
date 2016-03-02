eaa2c78bd09d646d2932e503946e3635f388215d
Catastrophe666 <catastronig@yahoo.com>
2016-03-02 14:23:12 -0600

    Fixing line spacing on comments, should be easier to read

--- Scroller-Gdx/core/src/com/mygdx/game/player.java
+++ Scroller-Gdx/core/src/com/mygdx/game/player.java
@@ -17,8 +17,8 @@ public class player
 	
 	// [Taj] Since we don't have any sprites yet we'll draw rectangles to 
 	// represent the player. Libgdx comes with something called a 
-	// shaperenderer that does most of the work for us. This is used 
-	// similarly to sprite batches.
+	// shaperenderer that does most of the work for us. This works similarly 
+	// like sprite batches.
 	ShapeRenderer playerBox; // [Taj] We will remove this once we have actual sprites.
 	
 	// [Taj] These are self explanatory.
@@ -35,10 +35,10 @@ public class player
 		// [Taj] Setup our player...
 		this.width = 32;
 		this.height = 32;
-		this.x = 200.0f; // [Taj] Java is a bit weird, you need to add "f" for 
-						 // a number be interpreted as a float. Since x and y
-						 // are floats, as we declared above, we need to do this.
-		this.y = 100.f;  // [Taj] So now the default coords are (200, 100)
+		this.x = 200.0f;	// [Taj] Java is a bit weird, you need to add "f" for 
+							// a number be interpreted as a float. Since x and y
+							// are floats, as we declared above, we need to do this.
+		this.y = 100.f;		// [Taj] So now the default coords are (200, 100)
 		
 		// [Taj] Uncomment this once we use actual sprites.
 		//batch = new SpriteBatch();
@@ -63,11 +63,11 @@ public class player
 		
 		playerBox.rect(x, y, width, height); // [Taj] This part makes the rectangle
 		
-		playerBox.end(); // [Taj] Because of how libgdx works we need this end part.
-						 // It basically accumilates everything we called between
-						 // begin() and end(), and then combines all of that into
-						 // one big call. Why? Because of how hardware is designed 
-						 // This style of rendering is efficient. It is better to
-						 // make one BIG call instead of multiple smaller calls.
+		playerBox.end();	// [Taj] Because of how libgdx works we need this end part.
+							// It basically accumilates everything we called between
+							// begin() and end(), and then combines all of that into
+							// one big call. Why? Because of how hardware is designed 
+							// This style of rendering is efficient. It is better to
+							// make one BIG call instead of multiple smaller calls.
 	}
 }
