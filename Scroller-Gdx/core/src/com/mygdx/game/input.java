a12ff3ed43e007415026ab372096dc8623e7e846
Catastrophe666 <catastronig@yahoo.com>
2016-03-02 14:38:25 -0600

    Added some example input detection to the input class

--- Scroller-Gdx/core/src/com/mygdx/game/input.java
+++ Scroller-Gdx/core/src/com/mygdx/game/input.java
@@ -1,10 +1,29 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+
 // [Taj] Use this class to handle all forms of input.
 public class input
 {
-	public input()
+	rockDodge game;
+	public input(rockDodge game)
 	{
+		this.game = game; // [Taj] You know the drill....
+	}
+	
+	public void update()
+	{
+		// [Taj] Does whatever is inside the brackets if the up arrow key is pressed.
+		if(Gdx.input.isButtonPressed(Input.Keys.UP))
+		{
+			
+		}
 		
+		// [Taj] Guess what this does :p
+		else if(Gdx.input.isButtonPressed(Input.Keys.DOWN))
+		{
+			
+		}
 	}
 }
