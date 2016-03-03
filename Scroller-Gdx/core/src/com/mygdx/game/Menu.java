4bb5bb70c2ac0acd10cb9515cd2a1bbe9a719f30
Jon Tran <Jon Tran>
2016-03-02 18:33:22 -0600

    Added a new class Menu.

---
+++ Scroller-Gdx/core/src/com/mygdx/game/Menu.java
@@ -0,0 +1,27 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+
+public class Menu {
+
+	private int curState; //Current state of the game
+	
+	
+	public Menu(rockDodge game)
+	{
+		this.curState = 0;
+	}
+	
+	
+	public void update()
+	{
+		
+	}
+	
+	public void render()
+	{
+		
+	}
+	
+}
