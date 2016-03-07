05a39ef51513f5965b6108fa40321d34eba1525e
Catastrophe666 <catastronig@yahoo.com>
2016-03-07 13:30:44 -0600

    Made small corrections to buttons class

--- Scroller-Gdx/core/src/com/mygdx/game/Buttons.java
+++ Scroller-Gdx/core/src/com/mygdx/game/Buttons.java
@@ -8,34 +8,34 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 
-public class Buttons extends rockDodge {
+public class Buttons
+{
 	Stage stage;
-    TextButton button;
-    TextButtonStyle textButtonStyle;
-    BitmapFont font;
-    Skin skin;
-    TextureAtlas buttonAtlas;
+	TextButton button;
+	TextButtonStyle textButtonStyle;
+	BitmapFont font;
+	Skin skin;
+	TextureAtlas buttonAtlas;
 
-    @Override
-    public void create() {      
-        stage = new Stage();
-        Gdx.input.setInputProcessor(stage);
-        font = new BitmapFont();
-        skin = new Skin();
-        buttonAtlas = new TextureAtlas(Gdx.files.internal("buttons/buttons.pack"));
-        skin.addRegions(buttonAtlas);
-        textButtonStyle = new TextButtonStyle();
-        textButtonStyle.font = font;
-        textButtonStyle.up = skin.getDrawable("up-button");
-        textButtonStyle.down = skin.getDrawable("down-button");
-        textButtonStyle.checked = skin.getDrawable("checked-button");
-        button = new TextButton("Button1", textButtonStyle);
-        stage.addActor(button);
-    }
+	public Buttons()
+	{
+		stage = new Stage();
+		Gdx.input.setInputProcessor(stage);
+		font = new BitmapFont();
+		skin = new Skin();
+		buttonAtlas = new TextureAtlas(Gdx.files.internal("buttons/buttons.pack"));
+		skin.addRegions(buttonAtlas);
+		textButtonStyle = new TextButtonStyle();
+		textButtonStyle.font = font;
+		textButtonStyle.up = skin.getDrawable("up-button");
+		textButtonStyle.down = skin.getDrawable("down-button");
+		textButtonStyle.checked = skin.getDrawable("checked-button");
+		button = new TextButton("Button1", textButtonStyle);
+		stage.addActor(button);
+	}
 
-    @Override
-    public void render() {      
-        super.render();
-        stage.draw();
-    }
+	public void render()
+	{
+		stage.draw();
+	}
 }
