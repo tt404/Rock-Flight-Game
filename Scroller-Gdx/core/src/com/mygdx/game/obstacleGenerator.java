b09787618ba4d62d9a6edbb257e6f2be8960f7ec
Catastrophe666 <catastronig@yahoo.com>
2016-03-09 14:02:05 -0600

    Added an obstacle generator class, will work on this solo

--- Scroller-Gdx/core/src/com/mygdx/game/obstacleGenerator.java
+++ Scroller-Gdx/core/src/com/mygdx/game/obstacleGenerator.java
@@ -5,7 +5,28 @@ import java.util.ArrayList;
 public class obstacleGenerator
 {
 	ArrayList<obstacle> obstacles;
+	
+	float initSpeed = 2.0f;
+	float curSpeed = 2.0f;
+	float maxSpeed = 24.0f;
+
+	float curTime = 0.0f;
+	float breakTime = 3.0f;
+	float timePerStage = 15.0f; // [Taj] 15 seconds
+	float maxGapSize = 196.0f;
+	float minGapSize = 48.0f;
+	
 	public obstacleGenerator()
+	{
+		
+	}
+	
+	public void generateRocks()
+	{
+		
+	}
+	
+	public void rockGenerationLoop()
 	{
 		
 	}
